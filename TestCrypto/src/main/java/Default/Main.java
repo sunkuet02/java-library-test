5d2f223eb588bd9629124193556d8c2b15f02007
Md. Abdulla-Al-Sun <sunkuet02@gmail.com>
2016-10-27 14:49:54 +0600

    encrypt and decrypt large data files

--- TestCrypto/src/main/java/Default/Main.java
+++ TestCrypto/src/main/java/Default/Main.java
@@ -2,22 +2,19 @@ package Default;
 
 import com.sun.org.apache.xml.internal.security.utils.Base64;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.io.FileInputStream;
-import java.io.IOException;
+import javax.crypto.*;
+import java.io.*;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 
 /**
  * Created by sun on 10/25/16.
  */
 public class Main {
-    public static void main(String args[]) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    public static void main(String args[]) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, ShortBufferException {
         String alias = "MRTD";
-        String certPath = "/media/sun/A4FE1C5EFE1C2AD6/EJBCA/TestCrypto/totalcertificate.p12";
+        String certPath = "/media/sun/20C1-A87E/java-library-test/TestCrypto/totalcertificate.p12";
 
         KeyStore keyStore = KeyStore.getInstance("pkcs12");
         keyStore.load(new FileInputStream(certPath),"tigerit".toCharArray());
@@ -47,5 +44,71 @@ public class Main {
         decrypt.init(Cipher.DECRYPT_MODE, privateKey);
         String decryptedMessage = new String(decrypt.doFinal(encryptedMessage), "UTF-8");
         System.out.println("Message : " + decryptedMessage);
+
+
+
+        System.out.println("=======================Encrypt-Decrypt Large Data=================================");
+
+        File file = new File("largeDataToEncrypt.txt");
+        byte[] bFile = new byte[(int) file.length()];
+        FileInputStream fileInputStream = new FileInputStream(file);
+        fileInputStream.read(bFile);
+        fileInputStream.close();
+
+        Main m = new Main();
+        byte[] encryptedData =  m.encryptDataBytes(publicKey, bFile);
+
+        System.out.println(Base64.encode(encryptedData) + "\n");
+
+        byte[] decryptedData = m.decryptDataBytes(privateKey, encryptedData);
+
+        System.out.println(new String(decryptedData) + "\n");
+
+    }
+
+    private byte[] encryptDataBytes(PublicKey publicKey, byte[] plainBytes) throws BadPaddingException, ShortBufferException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, IOException, KeyStoreException, InvalidKeyException {
+        Cipher enLarge = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        enLarge.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        int blockSize = enLarge.getBlockSize();
+        int outputSize = enLarge.getOutputSize(blockSize);
+        if(blockSize == 0) {
+            blockSize = outputSize -11;
+        }
+        int inLen = plainBytes.length;
+        int fullBlockCount = inLen / blockSize;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int inOffset = 0;
+        for (int i=0; i<fullBlockCount; i++) {
+            outputStream.write(enLarge.doFinal(plainBytes,inOffset,blockSize));
+            inOffset += blockSize;
+        }
+        if (inOffset < inLen) {
+            outputStream.write(enLarge.doFinal(plainBytes,inOffset,inLen-inOffset) );
+        }
+        return outputStream.toByteArray();
+    }
+
+    private byte[] decryptDataBytes(PrivateKey privateKey, byte[] encryptedBytes) throws BadPaddingException, ShortBufferException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, IOException, KeyStoreException, InvalidKeyException {
+        Cipher enLarge = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        enLarge.init(Cipher.DECRYPT_MODE, privateKey);
+
+        int blockSize = enLarge.getBlockSize();
+        int outputSize = enLarge.getOutputSize(blockSize);
+
+        int inLen = encryptedBytes.length;
+        int fullBlockCount = inLen / outputSize;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int inOffset = 0;
+        for (int i=0; i<fullBlockCount; i++) {
+            outputStream.write(enLarge.doFinal(encryptedBytes,inOffset,outputSize));
+            inOffset += outputSize;
+        }
+        if (inOffset < inLen) {
+            outputStream.write(enLarge.doFinal(encryptedBytes,inOffset,inLen-inOffset) );
+        }
+        return outputStream.toByteArray();
     }
 }
